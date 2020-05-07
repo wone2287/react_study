7e26c34b1685fddeed75c40326f6a5b99685a929
wone2287 <wone2287@gmail.com>
2020-05-07 18:23:13 +0900

    create method to save state at information array

--- src/App.js
+++ src/App.js
@@ -3,16 +3,31 @@ import PhoneComponent from './PhoneComponent';
 import './App.css';
 
 class App extends Component {
+  id = 2
+  state = {
+    information : [{
+      id : 1,
+      name : 'heo',
+      phone : '1234'
+    }]
+  };
 
   handleCreate = (data) => {
-    console.log(data);
+    const { information } = this.state;
+
+    this.setState({
+      'information' : information.concat({id : this.id++, ...data})
+    })
   }
 
   render(){
+    const { information } = this.state;
+
     return (
       <Fragment>
         <h3>test react</h3>
         <PhoneComponent onCreate={this.handleCreate}/>
+        {JSON.stringify(information)} 
       </Fragment>
     )
   }
